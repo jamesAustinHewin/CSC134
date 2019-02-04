f30debe1f83ddeb6b9d270115aa048303bba8606
jamesAustinHewin <46724911+jamesAustinHewin@users.noreply.github.com>
2019-02-03 21:48:39 -0500

    made three changes to M2LAB2
    1.  changed lower boundary from < 0.0 to <= 0.0
    2.  added do..while loop for input validation
    3.  added for loop to iterate 3 times for 3 separate grades

---
+++ M2LAB3_Hewin/main.cpp
@@ -0,0 +1,51 @@
+/*****************************************************************************/
+// James Austin Hewin
+// CSC-134
+// M2LAB3_Hewin
+// 02/03/2019
+/*****************************************************************************/
+#include <iostream>
+
+#define NUMBER_OF_GRADES 3
+
+using namespace std;
+/*****************************************************************************/
+int main()
+{
+    float numericalGrade = 0.0f;         // Initial numerical grade (0.0)
+    char letterGrade = 'F';              // Initial letter grade ('F')
+
+    cout << "************************************************************" << endl;
+    cout << "Please enter three numerical grades ( > 0 and <= 100 )" << endl;
+    for(int i = 0; i < NUMBER_OF_GRADES; i++) {      // Get 3 grades from user
+/*****************************************************************************/
+        do{
+            cout << "Type in the student's numerical grade " << i + 1 << ": "; // Prompt user for input
+            cin >> numericalGrade;                             // Store numerical grade
+/*****************************************************************************/
+        } while (numericalGrade <= 0.0 || numericalGrade > 100.0);
+
+        if(numericalGrade < 60.0) {       // below 60.0 => 'F'
+            letterGrade = 'F';
+        }
+        else if(numericalGrade < 70.0) {  // above/equal 60.0 AND below 70.0 => 'D'
+            letterGrade = 'D';
+        }
+        else if(numericalGrade < 80.0) {  // above/equal 70.0 AND below 80.0 => 'C'
+            letterGrade = 'C';
+        }
+        else if(numericalGrade < 90.0) {  // above/equal 80.0 AND below 90.0 => 'B'
+            letterGrade = 'B';
+        }
+        else {                            // above/equal 90.0 AND below/equal 100.0 => 'A'
+            letterGrade = 'A';
+        }
+/*****************************************************************************/
+        cout << "Numerical Grade: " << numericalGrade << "\tLetter Grade: ";
+        cout << letterGrade << endl;
+/*****************************************************************************/
+    cout << "************************************************************" << endl;
+
+    }
+    return 0;
+}
